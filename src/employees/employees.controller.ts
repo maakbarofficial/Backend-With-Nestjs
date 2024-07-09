ff88e2379f8f6d06ec017f85c09294778c09c639
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-09 20:30:40 +0500

    06_Lesson_Essentials

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,21 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Ip } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from "@prisma/client"
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my_logger/my_logger.service';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) { }
+  private readonly logger = new MyLoggerService(EmployeesController.name)
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({
+    default: false
+  }) // For Skipping Rate Limiting on routes below
   @Get()
-  findAll(@Query('role') role?: 'ADMIN' | 'HR' | 'USER') {
+  findAll(@Ip() ip: string, @Query('role') role?: 'ADMIN' | 'HR' | 'USER') {
+    this.logger.log(`Request for All Employees\t${ip}`, EmployeesController.name)
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my_logger/my_logger.service';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
+  app.useLogger(app.get(MyLoggerService))
+  app.setGlobalPrefix('api/v1') // http://localhost:3000/api/v1/users
+  app.enableCors(); //cors
   await app.listen(3000);
 }
 bootstrap();
